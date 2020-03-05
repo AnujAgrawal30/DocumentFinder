c0756f29724c9f2405228a981404190855596748
Anuj Agrawal <anujagrawal.official@gmail.com>
2020-03-05 22:38:33 +0530

    edited bat files

--- launcher.bat
+++ launcher.bat
@@ -1,3 +1,6 @@
-Call .\documentenv\Scripts\activate.bat
+Call .\documentenv\Scripts\activate.bat || goto :error
 start "" "website\index.html"
 python manage.py runserver
+:error
+echo "There was some error launching the web-page. Please ensure you have installed the project properly."
+cmd \k
--- setup.bat
+++ setup.bat
@@ -1,6 +1,10 @@
-python -m pip install --user virtualenv
-python -m venv documentenv
-Call .\documentenv\Scripts\activate
-python -m pip install -r requirements.txt
-echo "Installation Successful"
+python get-pip.py || goto :error
+python -m pip install --user virtualenv || goto :error
+python -m venv documentenv || goto :error
+Call .\documentenv\Scripts\activate || goto :error
+python -m pip install -r requirements.txt || goto :error
+echo "Installation Successful" && goto :end
+:error
+echo "Installation Unsuccessful"
+:end
 cmd \k
